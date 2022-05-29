2d08298b29a2a223627e719a8297232104ed4373
Nghia Le Van <nghialv@bunbusoft.com>
2022-05-29 12:33:56 +0900

    refs #0 play audio after waiting time

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -5,4 +5,12 @@ class HomesController < ApplicationController
     def playlist
         @music_settings = JSON.parse(cookies[:music_settings]) rescue {}
     end
+
+    def work
+        settings = JSON.parse(cookies[:music_settings]) rescue {}
+        @music_settings = MusicSetting.data.inject({}) do |h, e|
+            e[:music] = settings[e[:page].to_s].to_i if settings[e[:page].to_s]
+            h.merge(e[:page] => e)
+        end
+    end
 end
--- app/models/music_setting.rb
+++ app/models/music_setting.rb
@@ -3,95 +3,108 @@ class MusicSetting
     def data
       [
         {
-          page: "page1",
+          page: 1,
           scene: "一年目",
           in: "生まれ落ちた望月",
           out: "暗転",
-          music: 1
+          music: 1,
+          start_after: 2 # 6 seconds
         },
         {
-          page: "page2",
+          page: 3,
           scene: "循環の湖",
           in: "果てのない大きな湖",
           out: "みずのほしの大地へ還っていく。",
-          music: 2
+          music: 2,
+          start_after: 4 # 6 seconds
         },
         {
-          page: "page3",
+          page: 6,
           scene: "生き物全ての庭(翌日・夕方 〜夜))",
           in: "循環の湖のほど近く",
           out: "Usたち、歌い、踊り続けていている。",
-          music: 3
+          music: 3,
+          start_after: 6 # 6 seconds
         },
         {
-          page: "page4",
+          page: 15,
           scene: "循環の湖",
           in: "生まれた􏰅かりのusの頬に􏰆一本の 亀裂",
           out: "後ろ姿を見つめているI。",
-          music: 4
+          music: 4,
+          start_after: 6 # 6 seconds
         },
         {
-          page: "page5",
+          page: 20,
           scene: "囁きの高台",
           in: "座って木の実や葉など、自分が 集めた食材を食べている",
           out: "立ち上がり、また湖を眺める",
-          music: 5
+          music: 5,
+          start_after: 6 # 6 seconds
         },
         {
-          page: "page6",
+          page: 35,
           scene: "慟哭の木 (数日後・ 昼)",
           in: "チュンチュンと鳴く小鳥",
           out: "歩いていく",
-          music: 6
+          music: 6,
+          start_after: 6 # 6 seconds
         },
         {
-          page: "page7",
+          page: 40,
           scene: "至る川 (夜)",
           in: "焚火の前に座っているI。",
           out: "I、髪の毛を拭きながら、Meの後ろ姿を見ている",
-          music: 7
+          music: 7,
+          start_after: 6 # 6 seconds
         },
         {
-          page: "page11",
+          page: 45,
           scene: "空白の洞窟",
           in: "I 「......笑いたくない時があった」",
           out: "Me、洞窟の外を眺める。",
-          music: 8
+          music: 8,
+          start_after: 6 # 6 seconds
         },
         {
-          page: "page13",
+          page: 50,
           scene: "囁きの高台",
           in: "IとMe、なだらかな丘を歩いていく。",
           out: "新月。閉じていた目が開く。",
-          music: 9
+          music: 9,
+          start_after: 6 # 6 seconds
         },
         {
-          page: "page17",
+          page: 55,
           scene: "生き物全ての庭",
           in: "Iの後ろ姿を見ているMe。",
           out: "月の瞳、開いている。",
-          music: 10
+          music: 10,
+          start_after: 6 # 6 seconds
         },
         {
-          page: "page23",
+          page: 70,
           scene: "ひかりの道",
           in: "I「私たち􏰆あなた。あなた􏰆、わたし たち。わたし􏰆私。」",
           out: "ふたりの間を風が通り過ぎていく。",
-          music: 11
+          music: 11,
+          start_after: 6 # 6 seconds
         },
         {
-          page: "page24",
+          page: 80,
           scene: "生き物すべての庭",
           in: "(真夜中) I、踊りながら歌い始める",
           out: "Meにも、Iにも、􏰄sにも見える。",
-          music: 12
+          music: 12,
+          start_after: 6 # 6 seconds
         },
         {
-          page: "page24",
+          page: 90,
           scene: "生き物すべての庭",
           in: "静まり返ったみずのほしに、リンドウ 色の夜明けがやって来る。",
           out: "-",
-          music: 13
+          music: 13,
+          start_after: 6 # 6 seconds
         }
       ]
     end
