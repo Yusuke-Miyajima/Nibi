1e02a75184a43df6717ad2a44e1a2f4f196ff6c2
宮島佑典 <yuten7.09.01@gmail.com>
2022-06-26 21:22:58 +0900

    adjust music timing

--- app/models/music_setting.rb
+++ app/models/music_setting.rb
@@ -22,52 +22,52 @@ class MusicSetting
         },
         {
           id: 3,
-          page: 10,
+          page: 9,
           scene: "生き物全ての庭(翌日・夕方 〜夜))",
           in: "循環の湖のほど近く",
           out: "Usたち、歌い、踊り続けていている。",
           music: 3,
-          start_after: 1 # 1 seconds
+          start_after: 32 # 1 seconds
         },
         {
           id: 4,
-          page: 16,
+          page: 14,
           scene: "循環の湖",
           in: "生まれたばかりのusの頬には一本の亀裂",
           out: "後ろ姿を見つめているI。",
           music: 4,
-          start_after: 1 # 1 seconds
+          start_after: 14 # 1 seconds
         },
         {
           id: 5,
-          page: 20,
+          page: 18,
           scene: "囁きの高台",
           in: "座って木の実や葉など、自分が 集めた食材を食べている",
           out: "立ち上がり、また湖を眺める",
           music: 5,
-          start_after: 1 # 1 seconds
+          start_after: 16 # 1 seconds
         },
         {
           id: 6,
-          page: 24,
+          page: 23,
           scene: "慟哭の木 (数日後・ 昼)",
           in: "チュンチュンと鳴く小鳥",
           out: "歩いていく",
           music: 6,
-          start_after: 1 # 5 seconds
+          start_after: 34 # 5 seconds
         },
         {
           id: 7,
-          page: 30,
+          page: 28,
           scene: "至る川 (夜)",
           in: "焚火の前に座っているI。",
           out: "I、髪の毛を拭きながら、Meの後ろ姿を見ている",
           music: 7,
-          start_after: 1 # 1 seconds
+          start_after: 9 # 1 seconds
         },
         {
           id: 8,
-          page: 51,
+          page: 50,
           scene: "空白の洞窟",
           in: "I 「......笑いたくない時があった」",
           out: "Me、洞窟の外を眺める。",
@@ -76,30 +76,30 @@ class MusicSetting
         },
         {
           id: 9,
-          page: 60,
+          page: 58,
           scene: "囁きの高台",
           in: "IとMe、なだらかな丘を歩いていく。",
           out: "新月。閉じていた目が開く。",
           music: 9,
-          start_after: 10 # 10 seconds
+          start_after: 46 # 10 seconds
         },
         {
           id: 10,
-          page: 85,
+          page: 83,
           scene: "生き物全ての庭",
           in: "Iの後ろ姿を見ているMe。",
           out: "月の瞳、開いている。",
           music: 10,
-          start_after: 1 # 1 seconds
+          start_after: 59 # 1 seconds
         },
         {
           id: 11,
-          page: 112,
+          page: 114,
           scene: "ひかりの道",
           in: "I「私たちはあなた。あなたは、わたしたち。わたしは私。」",
           out: "ふたりの間を風が通り過ぎていく。",
           music: 11,
-          start_after: 20 # 1 seconds
+          start_after: 15 # 1 seconds
         },
         {
           id: 12,
